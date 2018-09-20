b264d938e3e32b181aca80c13a4ec35b1b1cf885
treyamador <tmamador@cpp.edu>
2018-09-20 03:34:07 -0700

    webscraper gathers html files and report.html

--- webscraper.py
+++ webscraper.py
@@ -1,17 +1,21 @@
 from bs4 import BeautifulSoup
 import requests
+import shutil
 import socket
 import time
 import os
 
 
+ERR_SHORT = 10
+ERR_LONG = 30
+MAX_ERRORS = 10
+
+CLOCK_ID = 0
+
+REPOSITORY_TOKEN = '.'
+
+
 def connect(url):
-
-    ERR_SHORT = 10
-    ERR_LONG = 30
-    MAX_ERRORS = 10
-
-    error_counter = 0
 
     def handle_errors(err, url, dur=0):
         print(err)
@@ -22,6 +26,7 @@ def connect(url):
         print('Successfully connected to', res.url)
         return BeautifulSoup(res.text, 'lxml'), res
 
+    error_counter = 0
     while error_counter < MAX_ERRORS:
         try:
             res = requests.get(url, timeout=1.0)
@@ -59,16 +64,26 @@ def connect(url):
     return None, None
 
 
+def init_dir():
+    report = 'report.html'
+    if os.path.exists(report):
+        os.remove(report)
+    repdir = 'repository/'
+    if os.path.exists(repdir):
+        shutil.rmtree(repdir)
+    os.mkdir(repdir)
+
+
 def init_seed(path):
     with open(path) as fobj:
-        url, pgs, rstr = fobj.readline().split(',')
+        url, pgs, rstr = fobj.readline().strip().split(',')
     if not url.startswith('http'):
-        url = 'http://' + url
+        url = 'https://' + url
     pgs = int(pgs) if pgs.isnumeric() else 100
     return url, pgs, rstr
 
 
-def get_robots(url):
+def get_robots_txt(url):
     roburl = url+'/robots.txt'
     if not url.startswith('http'):
         roburl = 'https://'+roburl
@@ -76,63 +91,106 @@ def get_robots(url):
     return bs.getText()
 
 
-def get_crawl_delay(url, delay=1):
+def get_crawl_delay(url):
     # make delay more specific
-    robot_txt = get_robots(url)
+    # pass dict of delay values
+    # return early if val present
+    robot_txt = get_robots_txt(url)
     lines = [x for x in robot_txt.split('\n') if x.startswith('Crawl-delay')]
     waits = [int(x.split(':')[1]) for x in lines]
-    waits.append(delay)
+    delay_def = 1
+    waits.append(delay_def)
     return max(waits)
 
 
-def gather_links(url, html):
-    # html, res = connect(url)
-    links = []
-    for a in html.find_all('a'):
-        if a.has_attr('href'):
-            a = a['href']
-            if a.startswith('//'):
-                a = 'http:'+a
-            elif a.startswith('/'):
-                a = url+a
-            links.append(a)
-    return links
+def init_loop():
+    return time.clock_gettime(CLOCK_ID)
 
 
-def write_html(url, res):
-    fpath = url.replace('/', '.')
+def end_loop(delay, seci):
+    dur_sec = time.clock_gettime(CLOCK_ID) - seci
+    pause_time = max(0, delay-dur_sec)
+    time.sleep(pause_time)
+    return pause_time
+
+
+def repository_fpath(url):
+    return url.replace('/', REPOSITORY_TOKEN)
+
+
+def write_html_file(url, res):
+    fpath = repository_fpath(url)
     with open('repository/'+fpath+'.html', 'wt') as fobj:
         fobj.write(res.text)
+
+
+def gather_links(html):
+    return [a['href'] for a in html.find_all('a') if a.has_attr('href')]
+
+
+def shape_links(url, links):
+    for i, a in enumerate(links):
+        if a.startswith('//'):
+            links[i] = 'https:' + a
+        elif a.startswith('/'):
+            links[i] = url + a
+    return links
 
 
 def scrape_page(url):
     html, res = connect(url)
     if html:
-        write_html(url, res)
-        links = gather_links(url, html)
-        return links
+        write_html_file(url, res)
+        links = gather_links(html)
+        return shape_links(url, links)
     return []
 
 
+def append_page_info(url, links):
+    with open('report.html', 'at') as fobj:
+        fpath = repository_fpath(url)
+        numlinks = str(len(links))
+        line = '<p>' + \
+            '<a href='+url+'>'+url+'</a> ' + \
+            '<a href='+'repository/'+fpath+'.html>'+fpath+'</a> ' + \
+            numlinks + '</p>'
+        fobj.write(line)
+
+
+def remove_restricted(links, rstr):
+    return list(filter(lambda x: rstr in x, links))
+
+
+def next_links(links, lnk):
+    links.extend(lnk)
+    return links.pop(0)
+
+
+def limit_not_reached(pages):
+    return len(os.listdir('repository/')) < pages
+
+
 def run():
+    init_dir()
     url, pgs, rstr = init_seed('specification.csv')
+
     links = []
+    directory = {}
 
-    delay = get_crawl_delay(url)
-    delay = 1
-
-    while len(os.listdir('repository/')) < pgs:
-
-        # init time, subtract max(delay-time_taken, 0)
-
+    while limit_not_reached(pgs):
+        sec_i = init_loop()
+        # delay = get_crawl_delay(url)
+        delay = 1
         lnk = scrape_page(url)
-        links.extend(lnk)
-        url, links = links[0], links[1:]
-        time.sleep(delay)
-
-        print(url)
+        append_page_info(url, lnk)
+        lnk = remove_restricted(lnk, rstr)
+        url = next_links(links, lnk)
+        end_loop(delay, sec_i)
 
 
 if __name__ == '__main__':
     run()
+
+
+# http://www.rainymood.com/
 
