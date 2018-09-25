40f7bbf5ca651606e6be1a59764de11ddc58ac31
treyamador <tmamador@cpp.edu>
2018-09-25 02:44:52 -0700

    multithreading webpages functioning

--- webscraper.py
+++ webscraper.py
@@ -1,4 +1,5 @@
 from bs4 import BeautifulSoup
+import threading
 import requests
 import shutil
 import socket
@@ -6,23 +7,25 @@ import time
 import os
 
 
-ERR_SHORT = 10
-ERR_LONG = 30
-MAX_ERRORS = 10
+ERR_SHORT = 1
+ERR_LONG = 3
+MAX_ERRORS = 3
 
 CLOCK_ID = 0
-
 REPOSITORY_TOKEN = '.'
+TOTAL_PAGES = 1000
 
 
 def connect(url):
 
     def handle_errors(err, url, dur=0):
+        # TODO uncomment print msg
         print(err)
         print('Resuming in ', dur, 'sec...')
         time.sleep(dur)
 
     def success_connect(res):
+        # TODO uncomment print msg
         print('Successfully connected to', res.url)
         return BeautifulSoup(res.text, 'lxml'), res
 
@@ -87,8 +90,10 @@ def get_robots_txt(url):
     roburl = url+'/robots.txt'
     if not url.startswith('http'):
         roburl = 'https://'+roburl
-    bs, res = connect(roburl)
-    return bs.getText()
+    tree, res = connect(roburl)
+    if tree:
+        return tree.getText()
+    return ''
 
 
 def get_crawl_delay(url):
@@ -104,11 +109,11 @@ def get_crawl_delay(url):
 
 
 def init_loop():
-    return time.clock_gettime(CLOCK_ID)
+    return time.time()
 
 
 def end_loop(delay, seci):
-    dur_sec = time.clock_gettime(CLOCK_ID) - seci
+    dur_sec = time.time() - seci
     pause_time = max(0, delay-dur_sec)
     time.sleep(pause_time)
     return pause_time
@@ -143,7 +148,7 @@ def scrape_page(url):
         write_html_file(url, res)
         links = gather_links(html)
         return shape_links(url, links)
-    return []
+    return None
 
 
 def append_page_info(url, links):
@@ -161,35 +166,71 @@ def remove_restricted(links, rstr):
     return list(filter(lambda x: rstr in x, links))
 
 
-def next_links(links, lnk):
-    links.extend(lnk)
-    return links.pop(0)
+def push_links(directory, key, links, restrict):
+    for link in links:
+        hostname = get_hostname(link)
+        if hostname not in directory.keys():
+            directory[hostname] = [link]
+            thr = threading.Thread(target=run_thread, args=(hostname, directory, restrict))
+            thr.start()
+        else:
+            directory[hostname].append(link)
 
 
-def limit_not_reached(pages):
-    return len(os.listdir('repository/')) < pages
+def next_url(links):
+    if len(links) > 0:
+        return links.pop(0)
+    return None
 
 
-def run():
-    init_dir()
-    url, pgs, rstr = init_seed('specification.csv')
+def limit_not_reached():
+    global TOTAL_PAGES
+    return len(os.listdir('repository/')) < TOTAL_PAGES
 
-    links = []
-    directory = {}
 
-    while limit_not_reached(pgs):
+def has_url(url):
+    return url is not None
+
+
+def get_hostname(url):
+    url_t = [u for u in url.split('/') if u]
+    if len(url_t) == 0:
+        return None
+    elif len(url_t) == 1:
+        return 'https://'+url_t[0]
+    elif url_t[0].startswith('http'):
+        return 'https://'+url_t[1]
+    else:
+        return 'https://'+url_t[0]
+
+
+def run_thread(hostname, directory, restrict):
+    url = directory[hostname].pop(0)
+    delay = get_crawl_delay(hostname)
+    while limit_not_reached() and has_url(url):
         sec_i = init_loop()
-        # delay = get_crawl_delay(url)
-        delay = 1
-        lnk = scrape_page(url)
-        append_page_info(url, lnk)
-        lnk = remove_restricted(lnk, rstr)
-        url = next_links(links, lnk)
+        links = scrape_page(url)
+        if links:
+            append_page_info(url, links)
+            links = remove_restricted(links, restrict)
+            push_links(directory, hostname, links, restrict)
+        url = next_url(directory[hostname])
         end_loop(delay, sec_i)
 
 
+def run_main():
+    url, pgs, rstr = init_seed('specification.csv')
+    links = [url]
+    directory = {}
+    global TOTAL_PAGES
+    TOTAL_PAGES = pgs
+    init_dir()
+    push_links(directory, '', links, rstr)
+
+
 if __name__ == '__main__':
-    run()
+    # run()
+    run_main()
 
 
 # http://www.rainymood.com/
