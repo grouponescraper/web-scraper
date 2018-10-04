c16e266396b8924bac25f49feabf2c47c40881d7
treyamador <tmamador@cpp.edu>
2018-10-04 00:48:18 -0700

    improve dense link filter fxn

--- filter.py
+++ filter.py
@@ -3,6 +3,9 @@ from lxml import html
 from lxml.html import HtmlElement, etree
 from lxml.html.clean import Cleaner
 from urllib import request
+import re
+import matplotlib.pyplot as plt
+import numpy as np
 
 
 def connect(url):
@@ -26,10 +29,6 @@ def connect(url):
 
 def retrieve_html(url):
     raw_html = connect(url)
-    # tree = lxml.html.etree.fromstring(raw_html)
-    # print(tree)
-    # print(type(tree))
-    # tree = lxml.html.etree.fromstring(raw_html)
     return lxml.html.fromstring(raw_html)
 
 
@@ -38,6 +37,7 @@ def clean(doc):
         scripts=True,
         javascript=True,
         style=True,
+        inline_style=True,
         comments=True,
         forms=True,
         frames=True,
@@ -46,8 +46,13 @@ def clean(doc):
         remove_unknown_tags=True,
         kill_tags=(
             'form',
-            'img',
-            'button'
+            'input',
+            'footer',
+            'button',
+            'embed',
+            'textarea',
+            # 'img',
+            # 'button'
         ),
         # remove_tags=(
         #     'span'
@@ -55,46 +60,132 @@ def clean(doc):
     ).clean_html(doc)
 
 
+def get_fmt_tags():
+    return [
+        'b', 'em', 'i', 'mark', 's', 'small', 'strong', 'sub', 'sup', 'u'
+    ]
 
-def traverse(doc):
-    # nodes to check == div, tr, p,
-    if isinstance(doc, HtmlElement):
-        # tree = doc.getchildren()
-        tree = doc.getroottree()
-        print(type(tree))
-        print('\n')
-        for node in tree.iter():
-            pass
-    return doc
+
+def get_struct_tags():
+    return [
+        'a', 'article', 'body', 'div',
+        'h1', 'h2', 'h3', 'h4', 'h5', 'h6',
+        'li', 'ol', 'p', 'q', 'section',
+        'span', 'table', 'tbody', 'td',
+        'title', 'tr', 'ul'
+    ]
 
 
 def remove_link_dense(doc):
-    tags = ['div', 'span', 'tr', 'p']
+    # TODO remove this?
     if isinstance(doc, HtmlElement):
         remove_nodes = []
+        tags = ['div', 'span', 'tr', 'p']
         for node in doc.iter(*tags):
             a_words = float(sum([len(a.text.split()) for a in node.iter('a') if a.text]))
             word_count = float(sum([len(t.text.split()) for t in node.iter() if t.text]))
-            if word_count == a_words != 0:
+            if a_words >= word_count != 0:
+                remove_nodes.append(node)
+                # print(word_count, [t.text.split() for t in node.iter() if t.text])
+                # print(a_words, [a.text.split() for a in node.iter('a') if a.text])
+                # print('\n')
+        for node in remove_nodes:
+            node.getparent().remove(node)
+    return doc
+
+
+def remove_link_tables(doc):
+    # TODO remove this?
+    if isinstance(doc, HtmlElement):
+        remove_nodes = []
+        tags = ['table', 'ul', 'ol', 'nav']
+        tags.extend(['div', 'span', 'p', 'tr'])
+        for node in doc.iter(*tags):
+            table = ''.join(node.text_content().split())
+            table_sum = float(len(table))
+            anchors = [''.join(a.text_content().split()) for a in node.iter('a')]
+            anchors_sum = float(sum([len(a) for a in anchors]))
+            if anchors_sum >= (3/4*table_sum) != 0:
                 remove_nodes.append(node)
         for node in remove_nodes:
             node.getparent().remove(node)
     return doc
 
 
+def remove_dense_link(doc, fctr=1.0):
+    if isinstance(doc, HtmlElement):
+        tags = ['div', 'span', 'p', 'tr', 'table', 'ul', 'ol', 'nav']
+        remove_nodes = []
+        for node in doc.iter(*tags):
+            table = ''.join(node.text_content().split())
+            table_sum = float(len(table))
+            anchors = [''.join(a.text_content().split()) for a in node.iter('a')]
+            anchors_sum = float(sum([len(a) for a in anchors]))
+            if anchors_sum >= fctr*table_sum != 0:
+                remove_nodes.append(node)
+        for node in remove_nodes:
+            node.getparent().remove(node)
+    return doc
+
+
+def document_slope_curve(doc):
+    # TODO update this to include a list of nodes to delete
+    TOKEN_TYPES = {
+        'word': 0,
+        'format': 0,
+        'structure': 1
+    }
+    tag_struct = get_struct_tags()
+    tag_fmt = get_fmt_tags()
+    slope = []
+    tokens = []
+    if isinstance(doc, HtmlElement):
+        for node in doc.iter():
+            tag, text = node.tag, node.text
+            if tag not in tag_fmt:
+                slope.append(TOKEN_TYPES['structure'])
+                tokens.append(tag)
+            else:
+                slope.append(TOKEN_TYPES['format'])
+                tokens.append(tag)
+            text = [] if text is None else text.split()
+            if tag in tag_struct and tag is not etree.Comment:
+                slope.extend([TOKEN_TYPES['word'] for x in text])
+                tokens.extend(text)
+
+        i = 1
+        for s, t in zip(slope, tokens):
+            print(i, ': ', s, ', ', t, sep='')
+            i += 1
+        print(len(slope), len(tokens))
+
+    return slope
+
+
 def noise_removal(url):
     doc = retrieve_html(url)
     if isinstance(doc, HtmlElement):
         doc = clean(doc)
-        doc = remove_link_dense(doc)
-        # print(doc.text_content())
+        doc = remove_dense_link(doc, 0.75)
     return doc
 
 
 def run_main():
     url = 'https://edition.cnn.com/2012/02/22/world/europe/uk-occupy-london/index.html'
+    # url = 'https://en.wikipedia.org/wiki/Peter_and_Paul_Fortress'
+
+    # doc = retrieve_html(url)
+    # slope = document_slope_curve(doc)
+    # cumslope = np.cumsum(slope)
+    # plt.plot(cumslope)
+    # plt.show()
+
     doc = noise_removal(url)
-    print(doc.text_content())
+
+    print('')
+    for node in doc.iter():
+        print(node.tag, node.text)
+    print('\n')
 
 
 if __name__ == '__main__':
